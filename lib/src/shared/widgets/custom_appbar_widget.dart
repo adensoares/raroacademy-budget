916f3f7e0a58f65f69b7bc8e73877f2f8bfb745e
Nilson Soares <nilsonsoares2011@gmail.com>
2021-08-03 00:34:28 -0300

    Cria uma appbar customizada única

---
+++ lib/src/shared/widgets/custom_appbar_widget.dart
@@ -0,0 +1,104 @@
+import 'package:budget/src/shared/constants/app_colors.dart';
+import 'package:budget/src/shared/constants/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatefulWidget implements PreferredSizeWidget {
+   CustomAppbar({ Key? key, this.hasFooter = true, this.height = 189 }) : super(key: key);
+
+  final bool hasFooter;
+  final double height;
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  State<CustomAppbar> createState() => _CustomAppbarState();
+}
+
+class _CustomAppbarState extends State<CustomAppbar> {
+  final String title = "";
+
+ String dropdownValue = 'Agosto'; 
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: AppColors.splashGradient
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16, left: 16.0, right: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: (){},
+                    icon: Icon(Icons.menu, color: Colors.white,)
+                  ),
+                  if(title != "Olá José")
+
+                  Text("Olá, José", style: AppTextStyles.white24w400Roboto,),
+
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border()
+                    ),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(
+                        color: Colors.red
+                      ),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['Agosto', 'Setembro', 'Outubro', 'Novembro']
+                        .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        })
+                        .toList(),
+                    ),
+                  )                                 
+                ],             
+              ),
+              if(widget.hasFooter)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text("Olá, José", style: AppTextStyles.white26w700Roboto,),
+                ],
+              ),
+              if(widget.hasFooter)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+               children: [
+                 TextButton(onPressed: (){}, child: Text("Entradas", style: AppTextStyles.white16w400Roboto,)),
+                 TextButton(onPressed: (){}, child: Text("Entradas", style: AppTextStyles.white16w400Roboto,)),
+                 TextButton(onPressed: (){}, child: Text("Entradas", style: AppTextStyles.white16w400Roboto,)),
+               ],
+              )
+            ],
+          ),
+        ),
+      ),
+      preferredSize: widget.preferredSize,
+    );
+  }
+}
