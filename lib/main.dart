958b02c742189908978b7a687a1979bdc7883ea1
Nilson Soares <nilsonsoares2011@gmail.com>
2021-08-05 02:49:09 -0300

    Torna a barra de status transparente

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:budget/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // transparent status bar
+  ));
   runApp(App());
 }
