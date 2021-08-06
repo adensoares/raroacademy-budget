774a6d064796d32820ab5e1edf401159c392187c
caio-deiro <caioguimaraes12@outlook.com>
2021-08-06 01:54:01 -0300

    menu lateral e adição do package mask na aplicação

--- lib/src/shared/widgets/appbar/simple_appbar_widget.dart
+++ lib/src/shared/widgets/appbar/simple_appbar_widget.dart
@@ -1,8 +1,13 @@
 import 'package:budget/src/shared/constants/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class SimpleAppbar extends StatelessWidget implements PreferredSizeWidget{
-  const SimpleAppbar({ Key? key, this.title = "", this.gradient, required this.expanded, }) : super(key: key);
+class SimpleAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const SimpleAppbar({
+    Key? key,
+    this.title = "",
+    this.gradient,
+    required this.expanded,
+  }) : super(key: key);
 
   final String title;
   final Gradient? gradient;
@@ -14,11 +19,10 @@ class SimpleAppbar extends StatelessWidget implements PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left:16.0, right: 16.0, top: 36.0, bottom: 16.0),
+      padding:
+          EdgeInsets.only(left: 16.0, right: 16.0, top: 36.0, bottom: 16.0),
       height: preferredSize.height,
-      decoration: BoxDecoration(
-        gradient: gradient
-      ),
+      decoration: BoxDecoration(gradient: gradient),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -28,29 +32,26 @@ class SimpleAppbar extends StatelessWidget implements PreferredSizeWidget{
             flex: 1,
             child: Align(
               alignment: Alignment.topLeft,
-              child: Builder(
-                builder: (context) {
-                  return IconButton(
-                    onPressed: ()=>Scaffold.of(context).openDrawer(),
-                    icon: Icon(Icons.menu, color: Colors.white,),
-                  );
-                }
-              ),
+              child: Builder(builder: (context) {
+                return IconButton(
+                  onPressed: () => Scaffold.of(context).openDrawer(),
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ),
+                );
+              }),
             ),
           ),
           Expanded(
-            flex: 2,
-            child: Text(
-              title,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center, 
-              style: AppTextStyles.white24w400Roboto,
-            )
-          ),
-          Expanded(
-            flex: 1,
-            child: Container()
-          )
+              flex: 2,
+              child: Text(
+                title,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: AppTextStyles.white26w700Roboto,
+              )),
+          Expanded(flex: 1, child: Container())
         ],
       ),
     );
