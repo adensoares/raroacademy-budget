988dd99e12796c16952fe06be9375b063de74d69
Nilson Soares <nilsonsoares2011@gmail.com>
2021-08-06 18:31:29 -0300

    Finalizado o dropdown customizado

--- lib/src/shared/widgets/custom_dropdown_widget.dart
+++ lib/src/shared/widgets/custom_dropdown_widget.dart
@@ -1,10 +1,13 @@
+import 'package:budget/src/shared/constants/app_colors.dart';
 import 'package:budget/src/shared/constants/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomDropdown extends StatefulWidget {
-  const CustomDropdown({ Key? key, required this.months, this.gradient }) : super(key: key);
+  const CustomDropdown({Key? key, required this.dropdownItens, this.gradient, required this.initialValue})
+      : super(key: key);
 
-  final List<String> months;
+  final List<String> dropdownItens;
+  final String initialValue;
   final Gradient? gradient;
 
   @override
@@ -12,40 +15,56 @@ class CustomDropdown extends StatefulWidget {
 }
 
 class _CustomDropdownState extends State<CustomDropdown> {
+
+  late String currentValue;
+
+  @override
+  void initState() {
+    currentValue = widget.initialValue; 
+    super.initState();    
+  }  
   @override
   Widget build(BuildContext context) {
-     String dropdownValue = 'Ago'; 
-    return Container(
-      padding: EdgeInsets.only(left: 8),
-      height: 30,
-      
-      decoration: BoxDecoration(
-        gradient: widget.gradient,
-        color: Colors.white.withOpacity(0.1),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
+    return Theme(
+      data: ThemeData(
+        canvasColor: AppColors.purple
       ),
-      child: DropdownButton<String>(
-        value: dropdownValue,
-        icon: const Icon(Icons.expand_more, color: Colors.white,),
-        iconSize: 24,
-        style: AppTextStyles.white14w500Roboto,
-        underline: Container(
-          height: 2,
-          color: Colors.transparent,
-        ), 
-        onChanged: (String? newValue) {
-          setState(() {
-            dropdownValue = newValue!;
-          });
-        },
-        items:   widget.months .map<DropdownMenuItem<String>>((String value) {
+      child: Container(
+        padding: EdgeInsets.only(left: 8),
+        height: 30,
+        decoration: BoxDecoration(
+          gradient: widget.gradient,
+          color: Colors.white.withOpacity(0.1),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: DropdownButton<String>(
+          value: currentValue,
+          icon: const Icon(
+            Icons.expand_more,
+            color: Colors.white,
+          ),
+          iconSize: 24,
+          style: AppTextStyles.white14w500Roboto,
+          underline: Container(
+            height: 2,
+            color: Colors.transparent,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              currentValue = newValue!;
+            });
+          },
+          items: widget.dropdownItens.map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
-              child: Text(value, style: AppTextStyles.black14w400Roboto,),
+              child: Text(
+                value,
+                style: AppTextStyles.white14w500Roboto,
+              ),
             );
-          })
-          .toList(),
+          }).toList(),
+        ),
       ),
-    );                               
+    );
   }
 }
