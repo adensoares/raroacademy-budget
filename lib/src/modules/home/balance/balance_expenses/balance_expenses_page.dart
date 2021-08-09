46935845f65fa9b8660c093a6a31c83c2fae56df
Nilson Soares <nilsonsoares2011@gmail.com>
2021-08-09 05:13:55 -0300

    Cria as páginas para a visualização das entradas, saídas e total

---
+++ lib/src/modules/home/balance/balance_expenses/balance_expenses_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:budget/src/modules/home/widgets/balances_card_page_widget.dart';
+import 'package:flutter/material.dart';
+
+class BalanceExpenses extends StatelessWidget {
+  const BalanceExpenses({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BalancesCardPage(
+      
+    );
+  }
+}
