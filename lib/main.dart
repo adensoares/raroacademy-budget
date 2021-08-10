a4e00a0f3598a9cf058e45bcdeae62d107886aff
caio-deiro <caioguimaraes12@outlook.com>
2021-08-10 19:58:54 -0300

    adicionando firebase ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:budget/firebase_app.dart';
 import 'package:budget/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-   WidgetsFlutterBinding.ensureInitialized();
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
-  runApp(App());
+  runApp(FirebaseWidget());
 }
