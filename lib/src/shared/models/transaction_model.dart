6eead187446d2f7f8cf3710f75f0d4dffb7a704b
Nilson Soares <nilsonsoares2011@gmail.com>
2021-08-13 03:56:47 -0300

    Alterados os models de usuário e transaçõe

--- lib/src/shared/models/transaction_model.dart
+++ lib/src/shared/models/transaction_model.dart
@@ -1,7 +1,51 @@
-abstract class Transaction {
+class Transaction {
 
-  String userID = "";
-  int price = 0;
-  DateTime date = DateTime.now();
+  String userId;
+  int price;
+  DateTime date;
+  String transactionName;
+  String? transactionDescription;
+  String transactionType;
+  String transactionCategory;
 
+  Transaction({
+    required this.userId,
+    required this.price,
+    required this.date,
+    required this.transactionName,
+    this.transactionDescription,
+    required this.transactionType,
+    required this.transactionCategory,
+  });
+
+
+  @override
+  String toString() {
+    return 'Transaction(userId: $userId, price: $price, date: $date, transactionName: $transactionName, transactionDescription: $transactionDescription, transactionType: $transactionType, transactionCategory: $transactionCategory)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Transaction &&
+      other.userId == userId &&
+      other.price == price &&
+      other.date == date &&
+      other.transactionName == transactionName &&
+      other.transactionDescription == transactionDescription &&
+      other.transactionType == transactionType &&
+      other.transactionCategory == transactionCategory;
+  }
+
+  @override
+  int get hashCode {
+    return userId.hashCode ^
+      price.hashCode ^
+      date.hashCode ^
+      transactionName.hashCode ^
+      transactionDescription.hashCode ^
+      transactionType.hashCode ^
+      transactionCategory.hashCode;
+  }
 }
--- lib/src/shared/models/user_model.dart
+++ lib/src/shared/models/user_model.dart
@@ -1,8 +1,92 @@
-class UserModel{
+import 'dart:convert';
+
+
+class UserModel {
+  final String userId ;
+  final String name;
+  final String email;
+  final String phone;
+  final String cpf;
+  final String password;
+
+  UserModel({
+    required this.userId,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.cpf,
+    required this.password,
+  });
+
+  UserModel copyWith({
+    String? userId ,
+    String? name,
+    String? email,
+    String? phone,
+    String? cpf,
+    String? password,
+  }) {
+    return UserModel(
+      userId: userId ?? this.userId,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phone: phone ?? this.phone,
+      cpf: cpf ?? this.cpf,
+      password: password ?? this.password,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'cpf': cpf,
+      'password': password,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      userId: map['userId'],
+      name: map['name'],
+      email: map['email'],
+      phone: map['phone'],
+      cpf: map['cpf'],
+      password: map['password'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'UserModel(userId: $userId, name: $name, email: $email, phone: $phone, cpf: $cpf, password: $password)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
   
-  final String name = "";
-  final String email = "";
-  final String phone = "";
-  final String cpf = "";
-  final String password = "";
+    return other is UserModel &&
+      other.userId == userId &&
+      other.name == name &&
+      other.email == email &&
+      other.phone == phone &&
+      other.cpf == cpf &&
+      other.password == password;
+  }
+
+  @override
+  int get hashCode {
+    return userId.hashCode ^
+      name.hashCode ^
+      email.hashCode ^
+      phone.hashCode ^
+      cpf.hashCode ^
+      password.hashCode;
+  }
 }
