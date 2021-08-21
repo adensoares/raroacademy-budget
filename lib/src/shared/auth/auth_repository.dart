d54ecda01c4f2d5ddb690d65774e5e0433ddc5b0
Aden Soares <adensoares@gmail.com>
2021-08-21 19:23:07 -0300

    Adicionado os binds referentes ao usuario no app_module

---
+++ lib/src/shared/auth/auth_repository.dart
@@ -0,0 +1,26 @@
+import 'package:budget/src/shared/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AuthRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserModel> getUser() async {
+    final userResponse = await FirebaseFirestore.instance
+        .collection('/users')
+        .doc(_auth.currentUser!.uid)
+        .get();
+    if (userResponse.data() != null) {
+      //Atribui os dados do userResponse ao user através o map do UserModel
+      UserModel user = UserModel.fromMap(userResponse.data()!);
+      user.userId = userResponse.data()!['uid'];
+      print('user abaixo');
+      print(user);
+      return user;
+    } else {
+      return UserModel(
+          userId: '', name: '', email: '', phone: '', cpf: '', password: '');
+    }
+  }
+}
